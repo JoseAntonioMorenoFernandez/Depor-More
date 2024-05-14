27bd53a21f272e6c53d87c708cabf88e5ebda01d
JoseAntonioMorenoFernandez <155090901+JoseAntonioMorenoFernandez@users.noreply.github.com>
2024-05-14 20:14:37 +0200

    Add files via upload

--- pagina/principal.php
+++ pagina/principal.php
@@ -39,7 +39,7 @@ $page = isset($_GET['page']) ? $_GET['page'] : '';
 <div class="row">
     <?php foreach ($articulosPagina as $articulo) : ?>
 
-        <div class="col-lg-4 col-md-6 col-sm-12 mt-1 mb-1">
+        <div class="col-xl-4 col-lg-6 col-md-6 col-sm-12 mt-1 mb-1">
             <div class="card h-100 overflow-hidden">
 
                 <div class="imagen-container">
@@ -49,20 +49,21 @@ $page = isset($_GET['page']) ? $_GET['page'] : '';
                 <div class="card-body d-flex flex-column">
                     <h5 class="card-title"><?php echo $articulo['nombre']; ?></h5>
                     <p class="card-text"><?php echo $articulo['descripcion']; ?></p>
-                    <div class="row mt-auto">
+                    
+                    <div class="row mt-auto pe-3">
                         <div class="col-5 mt-5">
-                            <h6 class="card-text fs-3 text-primary fw-bold text-decoration-none"><?php echo $articulo['precio']; ?> €</h6>
+                            <h6 class="card-text fs-4 text-primary fw-bold text-decoration-none"><?php echo $articulo['precio']; ?> €</h6>
                         </div>
                         <div class="col-5 mt-5">
                             <?php if ($articulo['descuento']) : ?>
-                                <h6 class="card-text fs-2 text-danger fw-bold text-decoration-none"> - <?php echo  $articulo['descuento']; ?> %</h6>
+                                <h6 class="card-text fs-4 text-danger fw-bold text-decoration-none"> - <?php echo  $articulo['descuento']; ?> %</h6>
                             <?php endif; ?>
                         </div>
                         <div class="col-2 text-end mt-4 ">
                             <form action="index.php?page=comprarArticulo" method="post">
                                 <input type="hidden" name="art_id" value="<?php echo $articulo['id_art']; ?>">
-                                <button type="submit" class="btn btn-primary p-2" aria-label="Carrito de la Compra">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16">
+                                <button type="submit" class="btn btn-primary p-2 me-4" aria-label="Carrito de la Compra">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16">
                                         <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5M3.14 5l.5 2H5V5zM6 5v2h2V5zm3 0v2h2V5zm3 0v2h1.36l.5-2zm1.11 3H12v2h.61zM11 8H9v2h2zM8 8H6v2h2zM5 8H3.89l.5 2H5zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0m9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0" />
                                     </svg>
                                     <span class="visually-hidden">Carrito de la Compra</span>
