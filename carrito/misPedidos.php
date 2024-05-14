704be7a06ede3ef27248ea219e10fb180f0b729c
JoseAntonioMorenoFernandez <155090901+JoseAntonioMorenoFernandez@users.noreply.github.com>
2024-05-14 17:53:22 +0200

    Add files via upload

---
+++ carrito/misPedidos.php
@@ -0,0 +1,8 @@
+<?php include './pagina/breadcrumb.php'; ?>
+<?php echo $breadcrumbs; ?>
+
+<div class="col-lg-12 col-md-8 col-sm-6 text-center py-5 ">
+    <h1>Mis Pedidos</h1>
+    <p class="mb-5 mt-5 fs-5">Listado pedidos</p>
+    
+</div>
